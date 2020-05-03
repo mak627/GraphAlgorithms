f4d8c3255a76a040eaa0842dfdb84f2bc6888bb9
mak627 <m.khadka69@gmail.com>
2020-05-03 17:57:42 +0700

    read no. of nodes from input file

--- BFS.cpp
+++ BFS.cpp
@@ -10,31 +10,39 @@ class Graph{
 		int numNodes;
 		list<int> *adjlist;
 	public:
-		void createGraph(int n);
+		void createGraph();
 		void BFS(int start);	
 };
 
-void Graph::createGraph(int n){
-	numNodes = n;
-	adjlist = new list<int>[n];
+void Graph::createGraph(){
+	//numNodes = n;
+	//adjlist = new list<int>[n];
 	string line;
 	int node,edge;
-	//istringstream iss;
 
 	ifstream inputG("input.txt");
 	if (inputG.is_open()){
-    	while ( getline (inputG,line) )
-    	{
-      	//iss.str(line);
-	istringstream iss(line);
-	iss>>node;
-	while(iss >> edge){
-		adjlist[node].push_back(edge);	
-	}
-	std::cout << node << endl;
-	for(auto it = adjlist[node].cbegin(); it != adjlist[node].cend();++it){
-		std::cout << *it << endl;
-	}
+    	while ( getline (inputG,line) ){
+		istringstream iss(line);
+		if(line[0]=='N'){
+			string n;
+			iss >> n;
+			iss >> numNodes;
+			iss.clear();
+			std::cout << "No. of Nodes = " << numNodes << endl;
+			adjlist = new list<int>[numNodes];
+			continue;
+		}
+		else{
+			iss>>node;
+			while(iss >> edge){
+				adjlist[node].push_back(edge);	
+			}
+		std::cout << node << endl;
+		for(auto it = adjlist[node].cbegin(); it != adjlist[node].cend();++it){
+			std::cout << *it << endl;
+		}
+		}
     	}
 	inputG.close();
 	}
@@ -58,14 +66,13 @@ void Graph::BFS(int start){
 		int node = queue.front();
 		queue.pop_front();
 		
-	for (auto it = adjlist[node].cbegin(); it != adjlist[node].cend();++it){ 
-            if (!visited[*it]) 
-            { 
+		for (auto it = adjlist[node].cbegin(); it != adjlist[node].cend();++it){ 
+            	if (!visited[*it]) { 
                 visited[*it] = true;
 		Parents[*it] = node;
                 queue.push_back(*it); 
-            } 
-        }		
+            	} 
+        	}		
 	}
 	for(int i=0;i<numNodes;i++){
 	std::cout << Parents[i] << ' ';	
@@ -75,7 +82,7 @@ void Graph::BFS(int start){
 int main(){
 		
 	Graph G;
-	G.createGraph(5);
+	G.createGraph();
 	G.BFS(0); 
 	return 0;
 }
